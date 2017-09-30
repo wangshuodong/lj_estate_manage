fdab97a33c350370cf56991247ce2998257ec11c
wangshuodong <wawngshuodong2004@163.com>
2017-09-30 17:58:06 +0800

    wangsd

--- src/com/ym/iadpush/manage/action/estate/EstateAction.java
+++ src/com/ym/iadpush/manage/action/estate/EstateAction.java
@@ -1,16 +1,19 @@
 package com.ym.iadpush.manage.action.estate;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayEcoCplifeBasicserviceInitializeRequest;
 import com.alipay.api.request.AlipayEcoCplifeBasicserviceModifyRequest;
 import com.alipay.api.request.AlipayEcoCplifeBillBatchUploadRequest;
@@ -1666,5 +1670,71 @@ public class EstateAction extends BaseAction {
         }
 
         return model;
+    }
+    
+    @RequestMapping("/alipay_web_return.do")
+    public void alipay_web_return(HttpServletRequest request,HttpServletResponse response) {
+      //获取支付宝POST过来反馈信息
+        Map<String,String> params = new HashMap<String,String>();
+        Map<String,String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext();) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            //乱码解决，这段代码在出现乱码时使用
+            //valueStr = new String(valueStr.getBytes("ISO-8859-1"), "utf-8");
+            params.put(name, valueStr);
+        }
+        CommonParm commonParm = new CommonParm();
+        String alipay_public_key = commonParm.getAlipay_public_key();
+        String charset = commonParm.getCharset();
+        String sign_type = commonParm.getSign_type();
+        try {
+            boolean signVerified = AlipaySignature.rsaCheckV1(params, alipay_public_key, charset, sign_type); //调用SDK验证签名
+            /* 实际验证过程建议商户务必添加以下校验：
+            1、需要验证该通知数据中的out_trade_no是否为商户系统中创建的订单号，
+            2、判断total_amount是否确实为该订单的实际金额（即商户订单创建时的金额），
+            3、校验通知中的seller_id（或者seller_email) 是否为out_trade_no这笔单据的对应的操作方（有的时候，一个商户可能有多个seller_id/seller_email）
+            4、验证app_id是否为该商户本身。
+            */
+            if(signVerified) {//验证成功
+                //商户订单号
+                String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"),"UTF-8");
+            
+                //支付宝交易号
+                String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"),"UTF-8");
+            
+                //交易状态
+                String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"),"UTF-8");
+                
+                if(trade_status.equals("TRADE_FINISHED")){
+                    //判断该笔订单是否在商户网站中已经做过处理
+                    //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                    //如果有做过处理，不执行商户的业务程序
+                        
+                    //注意：
+                    //退款日期超过可退款期限后（如三个月可退款），支付宝系统发送该交易状态通知
+                }else if (trade_status.equals("TRADE_SUCCESS")){
+                    //判断该笔订单是否在商户网站中已经做过处理
+                    //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
+                    //如果有做过处理，不执行商户的业务程序
+                    
+                    //注意：
+                    //付款完成后，支付宝系统发送该交易状态通知
+                }
+                response.getOutputStream().print("success");
+            }else {//验证失败
+                response.getOutputStream().print("fail");
+                //调试用，写文本函数记录程序运行情况是否正常
+                //String sWord = AlipaySignature.getSignCheckContentV1(params);
+                //AlipayConfig.logResult(sWord);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
